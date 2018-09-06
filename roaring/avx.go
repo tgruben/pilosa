51763a9a0d23d580402c1d5d9316e68da7b8cb07
Todd Gruben <todd@pilosa.com>
2018-09-06 10:56:40 -0500

    avx-512 exploration

--- roaring/avx.go
+++ roaring/avx.go
@@ -13,18 +13,22 @@ package roaring
 int avx_and( long long unsigned int* a_ptr, long long unsigned int* b_ptr, long long unsigned int* result){
 
    const long long unsigned int* a_ptr_end = a_ptr + 1024;
-   long long unsigned int temp[4];
+   long long unsigned int temp[8];
    int i=0;
    while (a_ptr < a_ptr_end) {
-      __m256i aa = _mm256_setr_epi64x(a_ptr[3], a_ptr[2], a_ptr[1], a_ptr[0]);
-      __m256i bb = _mm256_setr_epi64x(b_ptr[3], b_ptr[2], b_ptr[1], b_ptr[0]);
-       _mm256_store_si256((__m256i *)&temp[0], _mm256_and_si256(aa, bb));
+      __m512i aa = _mm512_setr_epi64(a_ptr[7],a_ptr[6],a_ptr[5],a_ptr[4],a_ptr[3], a_ptr[2], a_ptr[1], a_ptr[0]);
+      __m512i bb = _mm512_setr_epi64(b_ptr[7],b_ptr[6],b_ptr[5],b_ptr[4],b_ptr[3], b_ptr[2], b_ptr[1], b_ptr[0]);
+       _mm512_store_si512((__m512i *)&temp[0], _mm512_and_si512(aa, bb));
+      result[i++]=temp[7];
+      result[i++]=temp[6];
+      result[i++]=temp[5];
+      result[i++]=temp[4];
       result[i++]=temp[3];
       result[i++]=temp[2];
       result[i++]=temp[1];
       result[i++]=temp[0];
-      a_ptr += 4;
-      b_ptr += 4;
+      a_ptr += 8;
+      b_ptr += 8;
    }
    return 0;
 }
